3ba039ec3ba37f6fd63c89d2fd6e4fe9e285cc4b
dineshdalvi0507 <159693923+dineshdalvi0507@users.noreply.github.com>
2024-02-11 17:53:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.heading('Breakfast Menu')
+streamlit.header('Breakfast Menu',divider='rainbow')
+streamlit.header('Breakfast Menu')
