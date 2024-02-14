56b7febe8df2953296c1ba3cbae3df902ea2b5fc
dineshdalvi0507 <159693923+dineshdalvi0507@users.noreply.github.com>
2024-02-14 18:01:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favourites',divider='rainbow')
